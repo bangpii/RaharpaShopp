9419e83021f968e1769f62a2f1bfd26a7dcf7bdc
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-11-28 10:17:58 +0700

    first init 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import User from './pages/User';
-// import Admin from './pages/Admin';
-import ApiConnect from './services/ApiConnect';
+import User from './pages/User';
+import Admin from './pages/Admin';
 
 function App() {
   React.useEffect(() => {
@@ -23,12 +21,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ApiConnect />} />
-        <Route path="/api-test" element={<ApiConnect />} />
-        {/* <Route path="/user" element={<User />} /> */}
-        {/* <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/" element={<User />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-    </Router>
+    </Router>  // <- HAPUS HURUF "g" YANG NYASAR!!
   );
 }
 
